af5f93641f6c14ccd8985885067bae51d4bdb55b
Svek <alphanor14200@gmail.com>
2025-03-05 15:55:50 +0100

    🚧 (api) : add email sender config

--- web/collabenable/app.js
+++ web/collabenable/app.js
@@ -1,8 +1,24 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  host: "0.0.0.0",
+  port: 1025,
+  ignoreTLS: true,
+})
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
+    to: "alphanor14200@gmail.com",
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>",
+  })
+
   res.send('Hello World!')
 })
 
