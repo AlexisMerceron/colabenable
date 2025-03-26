865b02657da4df4db89d37b9ac634ce7d054b43f
kbontemps <nesta59440@gmail.com>
2025-03-26 15:49:34 +0100

    chore(env): add env file for backend

--- web/collabenable/app.js
+++ web/collabenable/app.js
@@ -1,65 +1,74 @@
-import express from 'express'
-import nodemailer from 'nodemailer'
-import cors from 'cors'
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const MAIL = 'MS_M6lMTb@trial-pr9084z0n38lw63d.mlsender.net'
-const PASSWORD = 'mssp.PhWti8z.0r83ql3jy1vgzw1j.1QYCcCi'
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.mailersend.net",
   port: 587,
   auth: {
-    user: MAIL,
-    pass: PASSWORD
-  }
-})
+    user: process.env.MAIL_FROM,
+    pass: process.env.PASSWORD,
+  },
+});
 
-// const transporter = nodemailer.createTransport({
-//   host: "0.0.0.0",
-//   port: 1025,
-// })
+//  const transporter = nodemailer.createTransport({
+//    host: "0.0.0.0",
+//    port: 1025,
+//  })
 
-const app = express()
-app.use(express.json({ limit: '10mb' }))
-app.use(cors({
-  origin: 'https://datacollect-beta.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}))
+const app = express();
+app.use(express.json({ limit: "10mb" }));
 
-const port = 3000
+if(process.env.NODE_ENV === "dev") {
+  app.use(cors());
+} else {
+  app.use(
+    cors({
+      origin: "https://datacollect-beta.vercel.app",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type"],
+    })
+  );
+}
 
-app.post('/send-data', async (req, res) => {
-  const { data, fileName } = req.body
+const port = 3000;
+
+app.post("/send-data", async (req, res) => {
+  const { data, fileName } = req.body;
 
   if (!data || !Array.isArray(data)) {
-    return res.status(400).json({ error: 'Invalid data format. Expected an array.' });
+    return res
+      .status(400)
+      .json({ error: "Invalid data format. Expected an array." });
   }
 
-  const csvContent = data.join('\n')
+  const csvContent = data.join("\n");
 
   await transporter.sendMail({
-    from: `"Collabenable" <${MAIL}>`,
-    to: "pralphanor@lrtechnologies.fr",
+    from: `"Collabenable" <${process.env.MAIL_FROM}>`,
+    to: process.env.MAIL_TO,
     subject: "📊 Nouvelle données [" + fileName + "] 🎉",
     text: "De nouvelles données ont été générées et sont disponibles en pièce jointe au format CSV.",
     html: "<b style='font-family: Arial, sans-serif;'>De nouvelles données ont été générées et sont disponibles en pièce jointe au format CSV 🤩.</b>",
     attachments: [
       {
-          filename: fileName,
-          content: csvContent,
-          contentType: 'text/csv'
-      }
-    ]
-  })
+        filename: fileName,
+        content: csvContent,
+        contentType: "text/csv",
+      },
+    ],
+  });
 
-  res.send('Hello World! true mail')
-})
+  res.send("Hello World! true mail");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-app.get('/version', async (req, res) => {
-  res.send('2.1.2')
-})
+app.get("/version", async (_, res) => {
+  res.send("2.1.2");
+});
