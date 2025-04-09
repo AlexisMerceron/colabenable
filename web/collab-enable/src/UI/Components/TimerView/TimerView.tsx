f03ce2c357c76680e8d201d379edb976e77ff432
LR Technologies Nantes <libellien12@lrtechnologies.fr>
2025-04-09 09:22:48 +0200

    Ajout de commentaire pour explication du fonctionnement

--- web/collab-enable/src/UI/Components/TimerView/TimerView.tsx
+++ web/collab-enable/src/UI/Components/TimerView/TimerView.tsx
@@ -3,10 +3,12 @@ import './TimerView.scss'
 import { TimeUtils } from '@utils/TimeUtils'
 import { FunctionComponent } from 'react'
 
+// Interface des propriétés du composant TimerView
 interface TimerViewProps {
-  seconds: number
+  seconds: number // Nombre de secondes à afficher
 }
 
+// Composant fonctionnel pour afficher un timer
 export const TimerView: FunctionComponent<TimerViewProps> = ({ seconds }) => {
   return <div className="TimerView">{TimeUtils.formatSeconds(seconds)}</div>
 }
